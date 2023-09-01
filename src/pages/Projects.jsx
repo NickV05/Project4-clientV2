5c3d4399ba2e1583c42d48e64e9c2e9a2de3110f
Nikita Valovyy <nickwolf046@gmail.com>
2023-08-31 23:55:43 -0400

    start

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,9 @@
-
+import { get, post } from "../services/routerService"
+import { useState, useEffect } from "react"
 
 const Projects = () => {
+
+  const [projects, setProjects] = useState(null)
   return (
     <div>
       
