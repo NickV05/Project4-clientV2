65152f7d9bbcedde590ac7d50d8d7a29252947a3
Nikita Valovyy <nickwolf046@gmail.com>
2023-09-12 18:13:28 -0400

    start over

--- src/services/routerService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export const get = (route) => {
-    return axios.get(import.meta.env.VITE_SERVER_URL + route)
-}
-
-export const post = (route, body) => {
-    return axios.post(import.meta.env.VITE_SERVER_URL + route, body)
-}
