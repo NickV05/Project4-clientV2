c2bf4774194c13c00506537ce9ec1757ca58e7ae
Nikita Valovyy <nickwolf046@gmail.com>
2023-09-14 23:22:46 -0400

    Navbar 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-const App = () => {
-  return (
-    <div>
-      <h2>Hello there</h2>
-    </div>
-  )
-}
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+import NavbarComp from "./components/NavbarComp";
+import HomePage from "./pages/homePage";
 
-export default App
+
+function App() {
+
+  const getToken = () => {
+    return localStorage.getItem('authToken')
+  }
+
+  const LoggedIn = () => {
+    return getToken() ? <Outlet /> : <Navigate to='/login' />
+  }
+
+  const NotLoggedIn = () => {
+    return !getToken() ? <Outlet /> : <Navigate to='/' />
+  }
+
+  return (
+    <div className="">
+      <NavbarComp />
+ 
+      <Routes>     
+
+        <Route path ="/" element={<HomePage/>}/>
+
+        {/* <Route element={<LoggedIn />}>
+
+
+        </Route> */}
+
+        {/* <Route element={<NotLoggedIn />}>
+          
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+
+        </Route> */}
+
+
+      </Routes>
+      
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/homePage.tsx
@@ -0,0 +1,10 @@
+const homePage = () => {
+
+  return (
+    <div>
+      <h2>This is homepage</h2>
+    </div>
+  )
+}
+
+export default homePage
