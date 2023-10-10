61b6fe5b01aa1b91c5496c673f0fd3835b776a19
Nikita Valovyy <nickwolf046@gmail.com>
2023-10-10 14:08:31 -0400

    burger 1

--- src/components/NavbarComp.tsx
+++ src/components/NavbarComp.tsx
@@ -18,18 +18,18 @@ const { logOutUser, isLoggedIn } = authContext;
                             <a className="navbar-brand py-3" href="/">
                                 <img src="/hosp.jpg" className =" w-1/3" />
                             </a>
-                            <button className="block navbar-toggler focus:outline-none lg:hidden" 
+                            { isLoggedIn && <button className="block navbar-toggler focus:outline-none lg:hidden" 
                             type="button" data-toggle="collapse" data-target="#navbarOne" aria-controls="navbarOne" 
                             aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="toggler-icon"></span>
                                 <span className="toggler-icon"></span>
                                 <span className="toggler-icon"></span>
-                            </button>
+                            </button>}
 
-                            <div className="absolute left-0 z-20 hidden w-full px-5 py-3 duration-300 
+                            { isLoggedIn &&<div className="absolute left-0 z-20 hidden w-full px-5 py-3 duration-300 
                             bg-white lg:w-auto collapse navbar-collapse lg:block top-full mt-full lg:static 
                             lg:bg-transparent shadow lg:shadow-none" id="navbarOne">
-                               { isLoggedIn && <ul id="nav" className="items-center content-start mr-auto lg:justify-end navbar-nav lg:flex">
+                                <ul id="nav" className="items-center content-start mr-auto lg:justify-end navbar-nav lg:flex">
                                     <li className="nav-item ml-5 lg:ml-11">
                                         <a className="page-scroll active" href="#home">Home</a>
                                     </li>
@@ -54,8 +54,8 @@ const { logOutUser, isLoggedIn } = authContext;
                                      <li className="nav-item ml-5 lg:ml-11">
                                         <button className="page-scroll"  onClick={logOutUser}><a>Log out</a></button>
                                     </li>
-                                </ul>}
-                            </div> 
+                                </ul>
+                            </div> }
                         </nav> 
                     </div>
                 </div> 
