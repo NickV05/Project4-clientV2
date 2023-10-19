0aa0872f935e4dd6a07e2d7777bdf2813ceef413
Nikita Valovyy <nickwolf046@gmail.com>
2023-10-19 14:34:28 -0400

    oops

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,7 +15,7 @@ function LoginPage() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
   const authContext = useContext(AuthContext);
   const navigate = useNavigate();
